aeac9195aa80c69c231d77bd16e821ebce54fe3f
Elias <gustafsson.ea@gmail.com>
2018-11-11 15:33:24 +0100

    GUI done.

--- Hangman/MainWindow.xaml.cs
+++ Hangman/MainWindow.xaml.cs
@@ -20,9 +20,42 @@ namespace Hangman
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int attempts;
+        private const int MAX_ATTEMPTS = 16;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            attempts = 0;
+        }
+
+        private void ResetGame()
+        {
+            attempts = 0;
+
+            foreach (Button btn in wp.Children.OfType<Button>())
+            {
+                btn.IsEnabled = true;
+                
+            }
+        }
+
+        // Add a button for each letter of the alphabet once the WrapPanel has been initialized
+        private void Wp_Initialized(object sender, EventArgs e)
+        {
+            const int ASCII_LETTER_INDEX = 65; // Start of alphabet in ascii
+
+            for (int i = 0; i < 26; i++)
+            {
+                System.Windows.Controls.Button addBtn = new Button
+                {
+                    Content = (char)(ASCII_LETTER_INDEX + i),
+                    Name = "button" + (char)(ASCII_LETTER_INDEX + i)
+                };
+
+                wp.Children.Add(addBtn);
+            }
         }
     }
 }
